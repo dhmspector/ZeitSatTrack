15c27efffb8ed9cde682679ebca6acb9698e22fe
David Spector <spector@zeitgeist.com>
2017-05-27 15:53:09 -0700

    WIP check in

--- ZeitSatTrackDemo/ViewController.swift
+++ ZeitSatTrackDemo/ViewController.swift
@@ -26,9 +26,41 @@ class ViewController: UIViewController, ZeitSatTrackManagerDelegate {
         satTracker.loadSatelliteCollectionForGroup(name: satGroups[1])
 
         
+        let globeViewC = theViewC as? WhirlyGlobeViewController
+        let mapViewC = theViewC as? MaplyViewController
+
+        theViewC = WhirlyGlobeViewController()
         self.view.addSubview(theViewC!.view)
         theViewC!.view.frame = self.view.bounds
         addChildViewController(theViewC!)
+
+        // we want a black background for a globe, a white background for a map.
+        theViewC!.clearColor = (globeViewC != nil) ? UIColor.black : UIColor.white
+        
+        // and thirty fps if we can get it ­ change this to 3 if you find your app is struggling
+        theViewC!.frameInterval = 2
+        
+        // set up the data source
+        if let tileSource = MaplyMBTileSource(mbTiles: "geography-class_medres"),
+            let layer = MaplyQuadImageTilesLayer(tileSource: tileSource) {
+            layer.handleEdges = (globeViewC != nil)
+            layer.coverPoles = (globeViewC != nil)
+            layer.requireElev = false
+            layer.waitLoad = false
+            layer.drawPriority = 0
+            layer.singleLevelLoading = false
+            theViewC!.add(layer)
+        }
+        
+        // start up over Madrid, center of the old-world
+        if let globeViewC = globeViewC {
+            globeViewC.height = 0.8
+            globeViewC.animate(toPosition: MaplyCoordinateMakeWithDegrees(-3.6704803, 40.5023056), time: 1.0)
+        }
+        else if let mapViewC = mapViewC {
+            mapViewC.height = 1.0
+            mapViewC.animate(toPosition: MaplyCoordinateMakeWithDegrees(-3.6704803, 40.5023056), time: 1.0)
+        }
 
     }
 
